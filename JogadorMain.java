8921bec5a0919da53c11c127cd922150f710419d
Lucas Nunes <lucassz_rj@hotmail.com>
2022-11-22 22:24:35 -0300

    Update JogadorMain.java
    Correção do método main do objeto Jogador. Irei fazer uma condição de parada em um futuro update.

--- JogadorMain.java
+++ JogadorMain.java
@@ -1,37 +1,65 @@
-import java.util.ArrayList;
+package principal;
+
 import java.util.Scanner;
-public class JogadorMain {
+
+import modelo.Jogador;
+
+public class mainJogador {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Jogador[] ArrrayList=new Jogador[40];
-		System.out.println("Insira o peso do respectivo jogador");
-		Scanner a=new Scanner(System.in);
-		double peso=a.nextDouble();
+		Scanner scan = new Scanner(System.in);
 		
+		String nome, posicao, pais;
+		double peso = 0.0, altura = 0.0;
 		
-		System.out.println("Insira a altura do respectivo jogador");
-		Scanner b=new Scanner(System.in);
-		double altura=b.nextDouble();
+		Jogador[] jogadores;
+		jogadores = new Jogador[40];
 		
-		
-		System.out.println("Insira o nome do jogador");
-		Scanner name= new Scanner(System.in);
-		String nome= name.nextLine();
-		
-		
-		System.out.println("Insira a posicao do jogador");
-		Scanner position=new Scanner(System.in);
-		String posicao=position.nextLine();
-		
-		
-		System.out.println("Insira a nacionalidade do jogador");
-		Scanner nacionalidad=new Scanner(System.in);
-		String nacionalidade=nacionalidad.nextLine();
-		
-		
-	  System.out.println( "\nDados do jogador:" + Jogador.toString() + "\nO IMC é: "+ Jogador.CalculaIMC(double altura,double peso));
+		for(int i=0; i<40; i++) {
+			jogadores[i] = new Jogador();
 		}
-	
+		
+		for(int i=0; i<40; i++) {
+			
+			System.out.println("Insira o nome do jogador: ");
+			nome = scan.nextLine();
+			jogadores[i].setNome(nome); 
+			
+			System.out.println("Insira a posicao do jogador: ");
+			posicao = scan.nextLine();
+			jogadores[i].setPosicao(posicao);
+			
+			System.out.println("Insira a nacionalidade do jogador: ");
+			pais = scan.nextLine();
+			jogadores[i].setPais(pais);
+			
+			System.out.println("Insira o peso do respectivo jogador: ");
+			peso = scan.nextDouble();
+			jogadores[i].setPeso(peso);
+			
+			System.out.println("Insira a altura do respectivo jogador:");
+			altura = scan.nextDouble();
+			jogadores[i].setAltura(altura);
+			
+			// QUER PARAR? "S" >> i = 40; pra parar o for.
+			
+		}
+		
+		for(int i=0; i<40; i++) {
+			jogadores[i].showData();
+			System.out.println(jogadores[i].IMC());
+		}
+		
+	}
 
 }
+
+/* 2. Crie uma classe para representar um jogador de futebol, com os
+atributos nome, posição, nacionalidade, altura e peso. Crie os métodos de
+acesso aos atributos (setters e getters) e também um método para imprimir
+todos os dados do jogador. Além disso, defina um método para calcular o
+IMC do jogador. Em seguida, crie uma classe com o método main utilizando
+array de objetos para armazenar os dados de até 40 jogadores, de acordo
+com o desejo do usuário. Ao final, o programa deve exibir todos os dados
+dos jogadores, bem como o IMC de cada um deles.*/
