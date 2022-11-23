2704e931a804655b1a64daac213d2ed25916dd3c
Lucas Nunes <lucassz_rj@hotmail.com>
2022-11-22 22:23:11 -0300

    Update Jogador.java
    Correção dos problemas encontrados no código do objeto Jogador

--- Jogador.java
+++ Jogador.java
@@ -1,46 +1,67 @@
+package modelo;
 
 public class Jogador {
-	String nome,posicao,nacionalidade;
+	String nome,posicao,pais;
 	double altura,peso;
 	
+	public Jogador(String nome, String posicao, String pais, double altura, double peso) {
+		this.nome = nome;
+		this.posicao = posicao;
+		this.pais = pais;
+		this.altura = altura;
+		this.peso = peso;
+	}
+	
+	public Jogador() {
+		
+	}
+
 	public String getNome() {
 		return nome;
 	}
-	public Jogador(){
 
-	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getPosicaoo() {
+
+	public String getPosicao() {
 		return posicao;
 	}
-	public void setPosicaoo(String posicao) {
+
+	public void setPosicao(String posicao) {
 		this.posicao = posicao;
 	}
-	public String getNacionalidade() {
-		return nacionalidade;
+
+	public String getPais() {
+		return pais;
 	}
-	public void setNacionalidade(String nacionalidade) {
-		this.nacionalidade = nacionalidade;
+
+	public void setPais(String pais) {
+		this.pais = pais;
 	}
+
 	public double getAltura() {
 		return altura;
 	}
-	public void setAltura(int altura) {
+
+	public void setAltura(double altura) {
 		this.altura = altura;
 	}
+
 	public double getPeso() {
 		return peso;
 	}
+
 	public void setPeso(double peso) {
 		this.peso = peso;
 	}
-	public String toString() {
-		return ( "\nNome: "+ nome+ "\nPosicao: "+ posicao+ "\nNacionalidade: "+nacionalidade+ "\nAltura: "+ altura + "\nPeso:" +peso );
+	
+	public double IMC() {
+		return(peso/(Math.pow(altura, 2)));
 	}
-	public static double CalculaIMC(double altura, double peso) {
-		return(peso/Math.pow(altura, 2));
+	
+	public void showData(){
+		System.out.print("Nome: "+nome+"\nPosição: "+posicao+"\nNacionalidade: "+pais+"\nPeso: "+peso+"\nAltura: "+altura);
+		System.out.println();
 	}
-
 }
