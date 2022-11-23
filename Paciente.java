6d877a6abd6a4a461d339f17de77bf915138dff7
Lucas Nunes <lucassz_rj@hotmail.com>
2022-11-23 11:47:19 -0300

    Create Paciente.java

---
+++ Paciente.java
@@ -0,0 +1,51 @@
+package modelo;
+
+public class Paciente {
+	int codigo;
+	String nome, CPF, telefone;
+	
+	public Paciente(int codigo, String nome, String CPF, String telefone) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.CPF = CPF;
+		this.telefone = telefone;
+	}
+	
+	public Paciente() {
+		
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
