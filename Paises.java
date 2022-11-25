2321bd4265fedc11ecea79e0547670c48090bf38
Lucas Nunes <lucassz_rj@hotmail.com>
2022-11-25 11:58:12 -0300

    Create Paises.java

---
+++ Paises.java
@@ -0,0 +1,59 @@
+package modelo;
+
+public class Pais {
+	private String sigla,nome;
+	private int populacao;
+	private double dimensao;
+	
+	public Pais(String sigla, String nome, int populacao, double dimensao) {
+		this.sigla=sigla;
+		this.nome=nome;
+		this.populacao=populacao;
+		this.dimensao=dimensao;
+	}
+	
+	public Pais() {
+		
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getPopulacao() {
+		return populacao;
+	}
+
+	public void setPopulacao(int populacao) {
+		this.populacao = populacao;
+	}
+
+	public double getDimensao() {
+		return dimensao;
+	}
+
+	public void setDimensao(double dimensao) {
+		this.dimensao = dimensao;
+	}
+	
+	public double densiPop() {
+		System.out.print("A densidade demográfica do país "+getNome()+" é de: ");
+		return getPopulacao() / getDimensao();
+	}
+	
+	public String toString() {
+		return "\nSigla: "+sigla+"\nNome: "+nome+"\nPopulação: "+populacao+"\nDimensão (em km²): "+dimensao+"\n";
+	}
+}
