3b84190e4f47d94fdaa85217fdfcc6cd8bca7d83
Lucas Nunes <lucassz_rj@hotmail.com>
2022-12-02 11:44:29 -0300

    Update Alunos.java
    Correção do código feito em sala de aula.

--- Alunos/src/Alunos.java
+++ Alunos/src/Alunos.java
@@ -1,15 +1,16 @@
-package modelo;
+package model;
 
 public class Aluno {
 	private long matricula;
 	private String nome;
-	private double nota1, nota2;
+	private double nota1, nota2, media;
 	
 	public Aluno(long matricula, String nome, double nota1, double nota2) {
 		this.matricula = matricula;
 		this.nome = nome;
 		this.nota1 = nota1;
 		this.nota2 = nota2;
+		this.media = this.calcMedia();
 	}
 	
 	public Aluno() {
@@ -48,12 +49,20 @@ public class Aluno {
 		this.nota2 = nota2;
 	}
 	
+	public double getMedia() {
+		return media;
+	}
+	
+	public void setMedia() {
+		this.media = calcMedia();
+	}
+	
 	public double calcMedia() {
-		return (nota1+nota2/2);
+		return ((nota1+nota2)/2);
 	}
 	
 	public boolean comparaNome(String nome) {
-		if(this.nome.equals(nome)) {
+		if(this.nome.equalsIgnoreCase(nome)) {
 			return true;
 		}else {
 			return false;
