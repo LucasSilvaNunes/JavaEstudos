435023c8107f6eca5bf6c35f23a90f99453e8f01
Lucas Nunes <lucassz_rj@hotmail.com>
2023-01-18 00:10:21 -0300

    Update Main.java

--- p2antiga/src/Main.java
+++ p2antiga/src/Main.java
@@ -1,21 +1,22 @@
 package imoveis;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
-    int i=0;
-    int num;
-    int resp;
-    System.out.println("Quantos imóveis deseja cadastrar? ");
-    num = keyboard.nextInt();
-    Imovel[] imoveis = new Imovel[num];
-    while (i < num){
-		System.out.println("1- Cadastrar um imovel novo\n2- Cadastrar um imovel usado");
+    int resp=0, ano;
+    double taxa;
+    String estado;
+    ArrayList <ImovelNovo> novos = new ArrayList<ImovelNovo>();
+    ArrayList <ImovelUsado> usados = new ArrayList<ImovelUsado>();
+    
+    while (resp != 3){
+		System.out.println("1- Cadastrar um imovel novo;\n2- Cadastrar um imovel usado;\n3- Exibir dados e sair;");
 		resp = keyboard.nextInt();
 		keyboard.nextLine();
-		if(resp ==1) {
-			Imovel im = new Imovel();
+		if(resp == 1) {
+			Imovel im = new ImovelNovo();
 			System.out.println("Local: ");
 			im.setLocalizacao(keyboard.nextLine());
 			System.out.println("Valor: ");
@@ -26,33 +27,42 @@ public class Main {
 			System.out.println("Proprietário: ");
 			im.setProp(keyboard.nextLine());
 			System.out.println("Taxa adicional: ");
-			double taxa = keyboard.nextDouble();
+			taxa = keyboard.nextDouble();
 			System.out.println("Estado: ");
-			String estado = keyboard.next();
-			imoveis[i] = new ImovelNovo(im,taxa,estado);
-			i++;
+			estado = keyboard.next();
+			ImovelNovo in = new ImovelNovo(im, taxa, estado);
+			novos.add(in);
 		}
 		else if(resp == 2) {
-			Imovel im = new Imovel();
+			Imovel im = new ImovelUsado();
 			System.out.println("Local: ");
 			im.setLocalizacao(keyboard.nextLine());
 			System.out.println("Valor: ");
 			im.setPreco(keyboard.nextDouble());
 			System.out.println("Quantidade de quartos: ");
 			im.setQtdquartos(keyboard.nextInt());
-			System.out.println("Proprietário: ");
-			im.setProp(keyboard.nextLine());	
-			System.out.println("Taxa de desconto: ");
-			double taxa = keyboard.nextDouble();
-			System.out.println("Ano: ");
-			int ano = keyboard.nextInt();
 			keyboard.nextLine();
-			imoveis[i] = new ImovelUsado(im,taxa,ano);
+			System.out.println("Proprietário: ");
+			im.setProp(keyboard.nextLine());
+			System.out.println("Taxa de desconto: ");
+			taxa = keyboard.nextDouble();
+			System.out.println("Ano: ");
+			ano = keyboard.nextInt();
+			keyboard.nextLine();
+			ImovelUsado iu = new ImovelUsado(im,taxa,ano);
+			usados.add(iu);
 		}
     }
-	System.out.println("cadastro de imoveis finalizado.");
-	for(int j=0;j<num;j++) {
-		System.out.println(imoveis[j]);
-	}
+    System.out.println("\nImoveis Usados:\n");
+    for(int i=0; i<usados.size(); i++) {
+    	System.out.println(usados.get(i).toString());
+    }
+    
+    System.out.println("\nImoveis Novos:\n");
+    for(int i=0; i<novos.size(); i++) {
+    	System.out.println(novos.get(i).toString());
+    }
+    
+	keyboard.close();
   }
 }
